76043bb7e7a7b2f2a399b3ba866a7b87ac9dacaf
kavyanshgangwar <kavyanshgangwar.cse18@itbhu.ac.in>
2022-01-17 21:49:38 +0530

    completed assignment 0

--- assignment.js
+++ assignment.js
@@ -1,11 +1,20 @@
+// area from which i have to convert to cypher text
 var cyphertext = document.getElementById('cyphertext');
+// area from which i have to convert to plain text
 var plaintext = document.getElementById('plaintext');
+// cypher text variable
 var cyStr = "";
+// plain text variable
 var plStr = "";
+// function to convert cypher text to plain text
 const cypherToPlain = function() {
+  // take input of user
   cyStr = cyphertext.value;
+  // intialize the palin text
   plStr = "";
+  // for every character in input use the algo for conversion
   for(var i=0;i<cyStr.length;i++){
+    // gets the ascii value of the character of string at index i
     var c = cyStr.charCodeAt(i);
     if(c>96 && c<123){
       c -= 97;
@@ -16,15 +25,22 @@ const cypherToPlain = function() {
       c = 25-c;
       c+=65;
     }
+    // converts ascii value to character
     plStr+=String.fromCharCode(c);
   }
+  // display the value obtained;
   plaintext.value = plStr;
 }
-
+// function to convert plain text to cypher text
 const plainToCypher = function(){
+  // take input of user
   plStr = plaintext.value;
+
+  // intialize the cypher text
   cyStr = "";
+  // for every character in input use the algo for conversion
   for(var i=0;i<plStr.length;i++){
+    // gets the ascii value of the character of string at index i
     var c = plStr.charCodeAt(i);
     if(c>96 && c<123){
       c -= 97;
@@ -35,9 +51,15 @@ const plainToCypher = function(){
       c = 25-c;
       c+=65;
     }
+    // converts ascii value to character
     cyStr+=String.fromCharCode(c);
   }
+  // display the value obtained;
   cyphertext.value = cyStr;
 }
+
+// adds an event listener to for conversion
 cyphertext.addEventListener('keyup',cypherToPlain);
+
+// adds an event listener to for conversion
 plaintext.addEventListener('keyup',plainToCypher);
